ba024097296e57a6b4fdab38384c604ab548e641
opiter09 <92452568+opiter09@users.noreply.github.com>
2023-02-25 23:08:01 -0500

    safety measure

--- apps/love/data/keybinds.lua
+++ apps/love/data/keybinds.lua
@@ -34,4 +34,10 @@ binds.player2 = {
 	["right"] = "Right"
 }
 
+for k, v in pairs(binds.player1) do
+	binds.player1[k] = string.upper(string.sub(v, 1, 1)) .. string.sub(v, 2)
+end
+for k, v in pairs(binds.player2) do
+	binds.player1[k] = string.upper(string.sub(v, 1, 1)) .. string.sub(v, 2)
+end
 return(binds)
