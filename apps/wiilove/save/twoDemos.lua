200aaa46f546f817523842c3bb8bd38fba941725
opiter09 <92452568+opiter09@users.noreply.github.com>
2023-03-01 16:08:46 -0500

    Update twoDemos.lua

--- apps/wiilove/save/twoDemos.lua
+++ apps/wiilove/save/twoDemos.lua
@@ -1,4 +1,6 @@
 begin = {}
+--demo scripts can include Images, Movements, Sounds, Objects, Color Changes, Variable Forks, Random Forks, Skill Jumps, Skill Calls, 
+--Skill Loops, and After Images
 
 function begin.update(dt, which)
 end
